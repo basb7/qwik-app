e8e156477a3de534e353ba97fc62c4ec67a27add
Brian <brianalbertosuarez@gmail.com>
2023-10-05 18:55:08 -0500

    Cambio de fuente y cambio en header (Sr y Sra)

--- src/components/starter/form/form.tsx
+++ src/components/starter/form/form.tsx
@@ -1,15 +1,17 @@
-import { component$, useSignal } from '@builder.io/qwik';
+import { component$, useSignal, $ } from '@builder.io/qwik';
 
 export default component$(() => {
 
   const showModal = useSignal(false);
   const inputCode = useSignal('')
 
+  const handleSetShowModal = $(() => {
+    showModal.value = !showModal.value
+  })
+
   return (
     <section>
-      <button onClick$={() => {
-        showModal.value = !showModal.value
-      }} class="mt-3 mx-auto block text- text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-[#DEAB69] font-medium rounded text-sm px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800" type="button">
+      <button onClick$={handleSetShowModal} class="mt-3 mx-auto block text- text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-[#DEAB69] font-medium rounded text-lg px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800" type="button">
         Confirmar asistencia
       </button>
 
@@ -18,24 +20,21 @@ export default component$(() => {
           <div class="fixed flex justify-center items-center backdrop-blur-sm top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%)] max-h-full">
             <div class="relative w-full max-w-md max-h-full">
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <button onClick$={() => {
-                      showModal.value = !showModal.value
-                    }} type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" >
+                    <button onClick$={handleSetShowModal} type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" >
                         <svg class="w-3 h-3" aria-hidden="true" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                         </svg>
                         <span class="sr-only">Close modal</span>
                     </button>
                     <div class="px-6 py-6 lg:px-8">
-                        <h3 class="mb-4 pt-10 text-xl font-medium text-gray-900 dark:text-white">Ingresa el código de verificación</h3>
+                        <h3 class="mb-4 pt-10 text-3xl font-medium text-gray-900 dark:text-white">Ingresa el código de verificación</h3>
                         <form class="space-y-6" action="#">
                             <div>
-                                <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Código de 4 digitos</label>
+                                <label for="number" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Código de 4 digitos</label>
                                 <input bind:value={inputCode} type="number" name="number" id="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm roundedfocus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="1234" required />
                             </div>
-                            <p>{inputCode.value}</p>
                             <div class="flex justify-between">
-                              <button type="submit" class="w-full text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-[#DEAB69] font-medium rounded text-sm px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">Login to your account</button>
+                              <button type="submit" class="w-full text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-[#DEAB69] font-medium rounded text-2xl px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">Enviar</button>
                             </div>
                         </form>
                     </div>
--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -4,7 +4,7 @@ export default component$(() => {
   return (
     <header class="z-10">
       <h1 class="text-yellow-700 text-4xl font-normal font-Cookie text-center pt-10 w-[70%] mx-auto">Se acerca la hora de ser</h1>
-      <p class="text-center text-5xl font-medium font-Cookie text-yellow-700">Mr & Mrs</p>
+      <p class="text-center text-5xl font-medium font-Cookie text-yellow-700">Sr & Sra</p>
       <p class="text-center text-4xl font-Cookie">¡Futuros esposos!</p>
     </header>
   );
